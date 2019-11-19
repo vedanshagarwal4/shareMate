36e376636cfdee94abbfe3fb808e1378405c3052
Abhijeet Mathur <abhijeetmathur786@gmail.com>
2019-11-19 18:06:10 +0530

    Some more edits

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
 	host : "localhost",
 	user : "root",
-	password : "04111998",
+	password : "Anujay786",
 	database : "shareMate"
 });
 
